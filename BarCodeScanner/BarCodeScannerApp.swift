c74ba1436c4fed7390d64f5943c2b8bb82858478
Nurul Hasan <mdnurulhasan1111@gmail.com>
2026-02-11 04:49:52 +0530

    setup

---
+++ BarCodeScanner/BarCodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarCodeScannerApp.swift
+//  BarCodeScanner
+//
+//  Created by Nurul Hasan on 11/02/26.
+//
+
+import SwiftUI
+
+@main
+struct BarCodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
